bb43c0a2e7eff2e4a91ff5ae6d7f29461a62c357
Kocken <i.j.kocken@uu.nl>
2022-12-19 16:41:29 +0100

    add more libraries for plotting to interactive file

--- R/libraries.R
+++ R/libraries.R
@@ -1,11 +1,18 @@
 library(tidyverse) # collection of packages dplyr, purrr, readr,
 library(glue)
 library(readxl)
+# remotes::install_github("isoverse/isoreader")
 library(isoreader)
+# remotes::install_github("isoverse/clumpedr")
 library(clumpedr)
 library(targets)
 library(slider)
 library(plotly) # not needed for workflow
+library(patchwork)
+library(gghighlight)
+library(ggforce)
 # I use tidylog for interactive sessions for nice logs, but I call it explicitly when desired
 library(gghighlight) # not needed for workflow
+
+# this used to be nice, but may be confusing
 sam <- scale_alpha_manual(values=c("TRUE" = 0.2, "FALSE" = 1))
